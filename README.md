b78adba746c53b999c8815d03feabcbcaec9e686
osadavc <osadavidath@gmail.com>
2022-03-22 16:13:31 +0530

    📝 Updated README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# NFT Pen
+# NFT Pen 🖊️
 
-Mint Your Code Pens As NFTs. See Them As Working Demos in NFT Marketplaces.
+Mint Your Code Pens As NFTs. See Them As Working Demos in NFT Marketplaces 🧠.
+
+## Local Setup
+
+Make sure to look at `.env.example` and add all your necessary values to a new `.env.local` file and run the project 👨🏻‍💻.
--- smart_contract/test/mintNFT.ts
+++ smart_contract/test/mintNFT.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("MyNFT", function () {
+describe("CodePen NFT", function () {
   it("Should mint and transfer an NFT to someone", async function () {
     const CodePenNFT = await ethers.getContractFactory("CodePenNFT");
     const codePenNFT = await CodePenNFT.deploy();
