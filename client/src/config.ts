1a62e697a3544b506b33e0b12d04ebaf71903725
osadavc <osadavidath@gmail.com>
2022-03-27 20:19:42 +0530

    ✨ Implemented Hasura JWT Shape

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,12 +4,16 @@ let PINATA_API_SECRET;
 let GOOGLE_CLIENT_ID;
 let GOOGLE_CLIENT_SECRET;
 
+let NEXTAUTH_SECRET;
+
 try {
   PINATA_API_KEY = process.env.PINATA_API_KEY;
   PINATA_API_SECRET = process.env.PINATA_API_SECRET;
 
   GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
   GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+  NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
 } catch (error) {}
 
 export const pinataAPIKey = PINATA_API_KEY ?? "";
@@ -17,6 +21,8 @@ export const pinataAPISecret = PINATA_API_SECRET ?? "";
 
 export const googleClientId = GOOGLE_CLIENT_ID ?? "";
 export const googleClientSecret = GOOGLE_CLIENT_SECRET ?? "";
+
+export const nextauthSecret = NEXTAUTH_SECRET ?? "";
 
 export const contractAddress =
   null ?? "0x04d4CC7fae00065Ebfd2422C064fa9615b18Ec13";
--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import jwt from "jsonwebtoken";
 
 import * as env from "config";
+import { JWT } from "next-auth/jwt";
 
 export default NextAuth({
   providers: [
@@ -10,4 +12,50 @@ export default NextAuth({
       clientSecret: env.googleClientSecret,
     }),
   ],
+  pages: {
+    signIn: "/",
+  },
+  jwt: {
+    encode: async ({ secret, token }) => {
+      return jwt.sign(
+        {
+          ...token,
+          userId: token?.id,
+          exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60 * 30,
+          iat: Math.floor(Date.now() / 1000),
+        },
+        secret
+      );
+    },
+    decode: async ({ secret, token }) =>
+      jwt.verify(token!, secret, { algorithms: ["HS256"] }) as JWT,
+  },
+  callbacks: {
+    async session({ session, token }) {
+      const encodedToken = jwt.sign(token, env.nextauthSecret, {
+        algorithm: "HS256",
+      });
+
+      session.id = token.id;
+      session.token = encodedToken;
+      return Promise.resolve(session);
+    },
+    async jwt({ token, user }) {
+      const isUserSignedIn = user ? true : false;
+
+      if (isUserSignedIn) {
+        token.id = user?.id.toString();
+        token.sub = user?.id.toString();
+        token.name = user?.name;
+        token.email = user?.email;
+        token["https://hasura.io/jwt/claims"] = {
+          "x-hasura-allowed-roles": ["user"],
+          "x-hasura-default-role": "user",
+          "x-hasura-role": "user",
+          "x-hasura-user-id": user?.id.toString(),
+        };
+      }
+      return Promise.resolve(token);
+    },
+  },
 });
